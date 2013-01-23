79e6f4113d7cbccde651a45c27692e1737488cb0
Edward Jiang <edwardrf@edwardrf-desktop.(none)>
2013-01-23 15:41:24 +0800

    Add nodejs express server. start basic binding.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,10 @@
 'use strict';
 
 /* Controllers */
+function millis(){
+	var d = new Date();
 
+}
 
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
@@ -10,3 +13,27 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function EditorCtrl($scope, frame){
+	$scope.timer = null;
+	$scope.lamps = frame.newFrame();
+
+	var st = 0;
+	var at = 0;
+
+	$scope.run = function(){
+		var s = Date.now();
+		frame.step($scope.lamps);
+		st += Date.now() - s;
+		s = Date.now();
+		$scope.$apply();
+		at += Date.now() - s;
+		$scope.timer = setTimeout($scope.run, 0);
+	};
+
+	$scope.stop = function(){
+		clearTimeout($scope.timer);
+		console.log([st, at]);
+	}
+}
+// EditorCtrl.$inject = ['$scope'];
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,36 @@
 
 /* Filters */
 
+function intToHexColor(input){
+	var tmp = ("00000" + input.toString(16));
+	tmp = tmp.substr(tmp.length - 6, 6);
+	return "#" + tmp;	
+}
+
+function offsetColor(input, offset){
+	var r = (input & 0xFF0000) >> 16;
+	var g = (input & 0x00FF00) >> 8;
+	var b = (input & 0x0000FF) >> 0;
+	r = r * (0xFF - offset) / 0xFF + offset;
+	g = g * (0xFF - offset) / 0xFF + offset;
+	b = b * (0xFF - offset) / 0xFF + offset;
+	var output = (r << 16) + (g << 8) + b;
+	return output;
+}
+
 angular.module('myApp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+    };
+  }]).filter('color', function() {
+  	return intToHexColor;
+  }).filter('lampColor', function(){
+  	return function(input){
+  		return intToHexColor(offsetColor(input, 0x66));
+  	};
+  }).filter('shadowColor', function(){
+  	return function(input){
+  		return intToHexColor(offsetColor(input, 0x66) - 0x222222);
+  	}
+  });
