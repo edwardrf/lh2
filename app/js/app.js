2fccfdbd34427c8a42050afcc1f22d5f12a3e0d4
edwardrf x200 <edwardrf@gmail.com>
2013-02-26 14:19:54 +0800

    make lamps not selectable started on colour selector allow frame size to change

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/edit', {templateUrl: 'partials/edit.html', controller: MyCtrl1});
-    $routeProvider.when('/play', {templateUrl: 'partials/play.html', controller: MyCtrl2});
-    $routeProvider.when('/export', {templateUrl: 'partials/export.html', controller: MyCtrl2});
+    $routeProvider.when('/edit', {templateUrl: 'partials/edit.html'});
+    $routeProvider.when('/play', {templateUrl: 'partials/play.html'});
+    $routeProvider.when('/export', {templateUrl: 'partials/export.html'});
     $routeProvider.otherwise({redirectTo: '/edit'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,18 +1,11 @@
 'use strict';
 
+/*global console */
+
 /* Controllers */
 function millis(){
 	var d = new Date();
-
 }
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
 
 function EditorCtrl($scope, frame){
 	$scope.timer = null;
@@ -34,6 +27,20 @@ function EditorCtrl($scope, frame){
 	$scope.stop = function(){
 		clearTimeout($scope.timer);
 		console.log([st, at]);
-	}
+	};
+
+	$scope.clk = function(a, b, e){
+		console.log(e);
+		if(e.which == 1) $scope.lamps[b][a] = 0xFFFFFF;
+	};
+
+	$scope.nodrag = function(e) {
+		e.preventDefault();
+	};
 }
 // EditorCtrl.$inject = ['$scope'];
+
+function ColorSelectGrayScaleCtrl($scope){
+	$scope.grayRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+	$scope.gray = 0;
+}
